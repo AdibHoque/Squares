96ea6032fa7807f2b34f808a93e193b7129860de
AdibHoque <adibhoque@gmail.com>
2023-01-30 14:57:45 +0600

    UPDATE

--- commands/help.js
+++ commands/help.js
@@ -5,6 +5,11 @@ function capitalize(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
   }
 
+  function errorEmbed(text) {
+    const embed = new EmbedBuilder().setDescription("<:Cross:1063031834713264128> "+text).setColor("#FF9900")
+    return embed;
+}
+
   module.exports.help = {
     name: "help",
     category: "Help",
@@ -26,7 +31,7 @@ module.exports.run = (client,interaction,options) => {
         try {
             command = require(`../commands/${cmd}.js`);
           } catch (e) {
-            return interaction.editReply("Please provide a valid command name!");
+            return interaction.editReply({embeds:[errorEmbed("Please provide a valid command name.")]});
           }
     const c = command.help
     return interaction.editReply({embeds:[new EmbedBuilder().setTitle("Command Help").setDescription(`<:Name:1066716238216187975> **Name:** \`${capitalize(c.name)}\`\n<:Description:1066716273821622303> **Description:** \`${c.description}\`\n<:Commands:1066347425465380924> **Category:** \`${c.category}\`\n<:Permissions:1066716314946773062> **Required Permissions:** \`${c.required}\`\n<:Usage:1066717996325814282> **Usage:** \`${c.usage}\``).setColor("#FF9900").setFooter({text:"Syntax [] = Optional | <> = Required"})]})
@@ -37,7 +42,7 @@ module.exports.run = (client,interaction,options) => {
     .setTitle(`<:Commands:1066347425465380924> HELP MENU`)
     .setDescription(`To get detailed help on a command, use \`/help <command name>\`!`)
     .setThumbnail("https://cdn.discordapp.com/attachments/678539559570767872/1063130454930239489/20230112_222006.gif")
-    .addFields([{name: "General", value:"`ping`, `avatar`, `define`, `color`, `math`, `winratecalc`, `userinfo`, `serverinfo`, `roleinfo`"}, {name: "Moderation", value:"`warn`, `logs`, `kick`, `ban`, `unban`, `nickname`, `resetnick`, `mute`, `unmute`, `role`"}, {name: "Entertainment", value:"`wordchain`, `tictactoe`"}, {name: "Giveaway", value:"`gstart`, `greroll`, `gend`, `gedit`, `gdelete`\n\n<:Plus:1063031875360280646> [Add BOT](https://discord.com/api/oauth2/authorize?client_id=629323586930212884&permissions=1513376050423&scope=bot%20applications.commands) | [Support Server](https://discord.gg/RDKvzEytHr)"}])
+    .addFields([{name: "General", value:"`ping`, `avatar`, `define`, `color`, `math`, `winratecalc`, `userinfo`, `serverinfo`, `roleinfo`"}, {name: "Moderation", value:"`warn`, `mute`, `unmute`, `kick`, `ban`, `unban`, `logs`, `loginfo`, `deletelog`, `clearlogs`, `nickname`, `resetnick`, `role`, `embed`"}, {name: "Entertainment", value:"`wordchain`, `tictactoe`"}, {name: "Giveaway", value:"`gstart`, `greroll`, `gend`, `gedit`, `gdelete`\n\n<:Plus:1063031875360280646> [Add BOT](https://discord.com/api/oauth2/authorize?client_id=629323586930212884&permissions=1513376050423&scope=bot%20applications.commands) | [Support Server](https://discord.gg/RDKvzEytHr)"}])
     .setColor(`#FF9900`)
 
     interaction.editReply({embeds: [embed]});
