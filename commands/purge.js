97f57a4501c54693fd5e0987dcaf7706c28720cf
AdibHoque <adibhoque@gmail.com>
2023-01-31 23:05:29 +0600

    Purge

---
+++ commands/purge.js
@@ -0,0 +1,57 @@
+const {SlashCommandBuilder, EmbedBuilder, PermissionFlagsBits} = require("discord.js");
+const g = require("../models/guild")
+
+
+function errorEmbed(text) {
+    const embed = new EmbedBuilder().setDescription("<:Cross:1063031834713264128> "+text).setColor("#FF9900")
+    return embed;
+}
+
+function successEmbed(text) {
+    const embed = new EmbedBuilder().setDescription("<:Check:1063031741482291220> "+text).setColor("#FF9900")
+    return embed;
+}
+
+
+module.exports.help = {
+    name: "purge",
+    category: "Moderationn",
+    description: "Bulk delete messages of a channel.",
+    required: "MANAGE_MESSAGES",
+    usage: "/purge <amount>"
+}
+
+module.exports.data = new SlashCommandBuilder()
+.setName("purge")
+.setDescription("Bulk delete messages of a channel.")
+.setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages)
+.addIntegerOption(option => option.setName("amount").setDescription("Amount of messages to delete. (Min-Max 1-99)").setRequired(true))
+
+
+module.exports.run = async(client,interaction,options) => {
+
+let amount = options.getInteger("amount") 
+if(amount > 99) amount = 99;
+if(amount < 1) amount = 1;
+
+interaction.channel.bulkDelete(amount+1, true)
+  .then(messages => {
+    interaction.channel.send({embeds:[successEmbed(`Successfully deleted ${messages.size-1} messages!`)]})
+    g.findOne({GuildID: interaction.guildId}, async (err, data) => {
+        if(err) throw err;
+        if(!data) return;
+        if(!data.LogChannel) return;
+        const c = interaction.guild.channels.cache.get(data.LogChannel);
+        if(!c) return;
+    
+    const embed = new EmbedBuilder().setTitle(`MESSAGES PURGED`).addFields([{name:`Moderator`, value:interaction.user.tag+" ("+interaction.user.id+")"}, {name:"Amount", value:`${messages.size-1} Messages`}, {name:`Channel`, value:"<#"+interaction.channel.id+">"}]).setThumbnail(interaction.guild.iconURL({ format: 'png', dynamic: true, size: 512 })).setTimestamp().setFooter({text:`MESSAGES PURGED`}).setColor(`#FF9900`)
+  c.send({embeds:[embed]})
+  })
+})
+  .catch(error => {
+    return interaction.editReply({embeds:[errorEmbed("Unable to purge messages!\nPlease check if i have enough permissions to execute this command.")]})
+})
+
+
+
+}
